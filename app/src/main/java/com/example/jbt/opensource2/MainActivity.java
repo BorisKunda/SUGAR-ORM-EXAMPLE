6920646e5d9875cfdaf87ec637ea565027d8a479
limudimwork <boriskunda@gmail.com>
2018-05-30 18:10:41 +0300

    check if record already exists

--- app/src/main/java/com/example/jbt/opensource2/MainActivity.java
+++ app/src/main/java/com/example/jbt/opensource2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jbt.opensource2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.List;
 //DONT FORGET ADD THIS TO MODULE INSIDE DEPENDENCIES : compile 'com.github.satyan:sugar:1.5'
@@ -13,7 +14,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Book book = new Book("Harry potter", "Rolling", 500);
+         Book book1 = new Book("Harry potter", "Rolling", 500);
+
+
+
+
+         //check if specific record already exists in database
+   /*    if( Book.find(Book.class," TITLE = 'Harry potter' ").isEmpty()){// if row with title HarryPotter  returns true on isEmpty method then there no such row in database
+         Log.e("true","true");
+       }else{
+           Log.e("false ","false");
+       }  */
 
 
 
